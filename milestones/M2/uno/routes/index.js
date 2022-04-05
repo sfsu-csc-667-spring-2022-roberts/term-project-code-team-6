44e3242dc7d82caaf52dae847ed0fe3e1e38b306
wye-ts <jacky274865077@gmail.com>
2022-04-04 18:07:16 -0700

    completed static-style sheets

--- milestones/M2/uno/routes/index.js
+++ milestones/M2/uno/routes/index.js
@@ -11,4 +11,23 @@ router.get('/how-to-play', function(req, res, next) {
   res.render('how-to');
 });
 
+/* GET how to play page. */
+router.get('/game/123', function(req, res, next) {
+  res.render('game');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
+});
+
+router.get('/terms&conditions')
+
+router.get('/terms&conditions', function(req, res, next) {
+  res.render('terms&conditions');
+});
+
 module.exports = router;
