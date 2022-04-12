bb1c79d09af73106d6f26d2e696e1841e1d73bac
wye-ts <jacky274865077@gmail.com>
2022-04-12 13:23:23 -0700

    updated game cards style

---
+++ milestones/M2/uno/public/javascripts/game.js
@@ -0,0 +1,43 @@
+const CARD_OFFSET = 30;
+const CARD_WIDTH = 80;
+const CARD_HEIGHT = 126;
+
+const playerCards = document.getElementsByClassName('hand');
+for (hand of playerCards) {
+	let right = 0;
+	for (card of hand.children) {
+		card.style.right = right + 'px';
+		right += CARD_OFFSET;
+	}
+}
+
+const p1 = playerCards.item(0);
+p1.style.transform = `translateX(${
+	((CARD_WIDTH - CARD_OFFSET) * (p1.children.length - 1) + CARD_WIDTH) / -2
+}px)`;
+
+const p2 = playerCards.item(1);
+const p2width =
+	(CARD_WIDTH - CARD_OFFSET) * (p2.children.length - 1) + CARD_WIDTH;
+const p2offsetY = -126 + p2width - p2width / 2;
+p2.style.transform = `translateY(${
+	-CARD_HEIGHT - p2width / 2
+}px) rotate(90deg)`;
+
+const p3 = playerCards.item(2);
+const p3width =
+	(CARD_WIDTH - CARD_OFFSET) * (p3.children.length - 1) + CARD_WIDTH;
+const p3offsetY = -126 + p3width - p3width / 2;
+p3.style.transform = `translateY(${p3offsetY}px) translateX(${
+	CARD_WIDTH * p3.children.length
+}px) rotate(-90deg)`;
+
+const userCards = playerCards.item(3);
+userCards.style.transform = `translateX(${
+	((CARD_WIDTH - CARD_OFFSET) * (userCards.children.length - 1) + CARD_WIDTH) /
+	-2
+}px)`;
+
+// translateY(${
+// 	((CARD_WIDTH - CARD_OFFSET) * (p1.children.length - 1) + CARD_WIDTH) / -2
+// }px)
--- milestones/M2/uno/routes/index.js
+++ milestones/M2/uno/routes/index.js
@@ -2,32 +2,39 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', teamNumber: "D" });
+router.get('/', function (req, res, next) {
+	res.render('index', { title: 'Express', teamNumber: 'D' });
 });
 
 /* GET how to play page. */
-router.get('/how-to-play', function(req, res, next) {
-  res.render('how-to');
+router.get('/how-to-play', function (req, res, next) {
+	res.render('how-to');
 });
 
 /* GET how to play page. */
-router.get('/game/123', function(req, res, next) {
-  res.render('game');
+router.get('/game/123', function (req, res, next) {
+	res.render('game', {
+		playerCards: {
+			p1: Array(7).fill(0),
+			p2: Array(7).fill(0),
+			p3: Array(7).fill(0),
+			user: Array(7).fill(0),
+		},
+	});
 });
 
-router.get('/login', function(req, res, next) {
-  res.render('login');
+router.get('/login', function (req, res, next) {
+	res.render('login');
 });
 
-router.get('/signup', function(req, res, next) {
-  res.render('signup');
+router.get('/signup', function (req, res, next) {
+	res.render('signup');
 });
 
-router.get('/terms&conditions')
+router.get('/terms&conditions');
 
-router.get('/terms&conditions', function(req, res, next) {
-  res.render('terms&conditions');
+router.get('/terms&conditions', function (req, res, next) {
+	res.render('terms&conditions');
 });
 
 module.exports = router;
