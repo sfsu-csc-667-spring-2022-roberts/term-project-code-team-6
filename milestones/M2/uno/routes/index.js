1afc55bc6926f3ef987dc77ae7f8912544354e58
wye-ts <jacky274865077@gmail.com>
2022-04-16 22:36:09 -0700

    connected backend to the landing page

--- milestones/M2/uno/routes/index.js
+++ milestones/M2/uno/routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const authUser = require('../middleware/isAuth');
 const router = express.Router();
+const db = require('../db');
 
 /* GET home page. */
-router.get('/', authUser, function (req, res, next) {
-	res.render('index', { title: 'UNO', teamNumber: 'D' });
+router.get('/', authUser, async function (req, res, next) {
+	let query = 'SELECT id, "userCount" FROM games;';
+	const games = await db.manyOrNone(query, []);
+	console.log(games);
+	res.render('index', { title: 'UNO', games: games });
 });
 
 /* GET how to play page. */
