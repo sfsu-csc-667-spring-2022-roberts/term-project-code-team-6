35467d45471eb2c1fb4216445e7ce1f01d9f3de5
wye-ts <jacky274865077@gmail.com>
2022-04-22 10:10:14 -0700

    remove unuserd next

--- milestones/M2/uno/routes/index.js
+++ milestones/M2/uno/routes/index.js
@@ -5,7 +5,7 @@ const db = require('../db');
 const isAuth = require('../middleware/isAuth');
 
 /* GET home page. */
-router.get('/', authUser, async function (req, res, next) {
+router.get('/', authUser, async function (req, res) {
 	let query = 'SELECT id, "userCount" FROM games;';
 	const games = await db.manyOrNone(query, []);
 	query = 'SELECT COUNT(id) FROM games;';
@@ -15,23 +15,23 @@ router.get('/', authUser, async function (req, res, next) {
 });
 
 /* GET how to play page. */
-router.get('/how-to-play', function (req, res, next) {
+router.get('/how-to-play', function (req, res) {
 	res.render('how-to');
 });
 
-router.get('/login', function (req, res, next) {
+router.get('/login', function (req, res) {
 	res.render('login');
 });
 
-router.get('/signup', function (req, res, next) {
+router.get('/signup', function (req, res) {
 	res.render('signup');
 });
 
-router.get('/terms&conditions', function (req, res, next) {
+router.get('/terms&conditions', function (req, res) {
 	res.render('terms&conditions');
 });
 
-router.get('/userInfo', isAuth, async function (req, res, next) {
+router.get('/userInfo', isAuth, async function (req, res) {
 	const userId = req.session.userId;
 	let query = 'SELECT username FROM users WHERE id = $1';
 	const fetchedUser = await db.one(query, [userId]);
