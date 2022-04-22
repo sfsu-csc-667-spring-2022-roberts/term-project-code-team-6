a938c78440b5ab74dc57fd69add04ebb934d14d5
wye-ts <jacky274865077@gmail.com>
2022-04-22 10:45:39 -0700

    updated drawCard with any draw count

--- milestones/M2/uno/db/cardDao.js
+++ milestones/M2/uno/db/cardDao.js
@@ -7,15 +7,25 @@ async function drawCards(gameId, userId, count) {
 			WHERE game_id = $1 AND user_id IS null AND discarded = false\
 			ORDER BY "order"\
 			LIMIT $2;';
-	const cardToAssgin = await db.oneOrNone(query, [gameId, count]);
+	const cardToAssgin = await db.manyOrNone(query, [gameId, count]);
 	console.log(cardToAssgin);
 
-	query =
-		'UPDATE game_cards\
-			SET user_id = $1\
-			WHERE game_id = $2 AND card_id = $3;';
-	console.log('Assgining card to user: ', userId);
-	await db.any(query, [userId, gameId, cardToAssgin.card_id]);
+	if (!cardToAssgin) {
+		console.log('No cards in the deck');
+		return { cardToAssgin };
+	}
+
+	for (let i = 0; i < count; i++) {
+		query =
+			'UPDATE game_cards\
+                SET user_id = $1\
+                WHERE game_id = $2 AND card_id = $3;';
+		console.log(`Assgining card ${cardToAssgin[i].card_id} to user: ${userId}`);
+		await db.any(query, [userId, gameId, cardToAssgin[i].card_id]);
+	}
+	return {
+		cardToAssgin,
+	};
 }
 
 module.exports = {
