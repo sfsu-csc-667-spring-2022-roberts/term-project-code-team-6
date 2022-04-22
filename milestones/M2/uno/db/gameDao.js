f00ecb9c606a9eccaaf89e2fca30f099d4773f34
wye-ts <jacky274865077@gmail.com>
2022-04-22 10:08:38 -0700

    added gameDao.js and checkUserTurn function

---
+++ milestones/M2/uno/db/gameDao.js
@@ -0,0 +1,30 @@
+const db = require('../db');
+
+async function checkUserTurn(gameId, userId) {
+	let query =
+		'SELECT user_id\
+    FROM game_users\
+    WHERE game_id = $1\
+    ORDER BY player_order;';
+	const userIdList = await db.any(query, [gameId]);
+	console.log(userIdList);
+
+	const userIndex = userIdList.findIndex(uid => uid.user_id === userId);
+
+	query = 'SELECT *\
+            FROM games\
+            WHERE id = $1';
+	const fetchedGame = await db.one(query, [gameId]);
+	console.log('Game info: ', fetchedGame);
+	console.log('Total discarded cards: ', fetchedGame.discardedCount);
+
+	return {
+		isUserTurn: userIndex === fetchedGame.player_turn,
+		userIdList,
+		fetchedGame,
+	};
+}
+
+module.exports = {
+	checkUserTurn,
+};
