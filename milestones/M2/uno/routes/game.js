f4b15aa2c978a493ea99cea8750699aeaa593033
wye-ts <jacky274865077@gmail.com>
2022-04-21 17:20:48 -0700

    added draw card route

--- milestones/M2/uno/routes/game.js
+++ milestones/M2/uno/routes/game.js
@@ -392,4 +392,36 @@ router.post('/:id/play/:cardId', authUser, async (req, res, next) => {
 	}
 });
 
+// Draw a card
+router.post('/:id/draw', authUser, async (req, res, next) => {
+	try {
+		const gameId = req.params.id;
+		const userId = req.session.userId;
+		console.log('gameId is: ', gameId);
+		console.log('userId is: ', userId);
+
+		let query =
+			'SELECT card_id\
+			FROM public.game_cards\
+			WHERE game_id = $1 AND user_id IS null AND discarded = false\
+			ORDER BY "order"\
+			LIMIT 1;';
+		const cardToAssgin = await db.oneOrNone(query, [gameId]);
+		console.log(cardToAssgin);
+
+		query =
+			'UPDATE game_cards\
+			SET user_id = $1\
+			WHERE game_id = $2 AND card_id = $3;';
+		console.log('Assgining card to user: ', userId);
+		await db.any(query, [userId, gameId, cardToAssgin.card_id]);
+
+		res.status(201).json({
+			message: `card ${cardToAssgin.card_id} is drew`,
+		});
+	} catch (err) {
+		next(err);
+	}
+});
+
 module.exports = router;
